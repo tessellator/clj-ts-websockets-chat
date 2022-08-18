916c622e7629c8119e581379034e4c96dda0ef15
Chad Taylor <chadtaylor@hey.com>
2022-08-18 14:14:21 -0500

    Swap from jetty to undertow

--- src/server/core.clj
+++ src/server/core.clj
@@ -1,7 +1,8 @@
 (ns server.core
   (:require [bell.core :refer [ANY GET POST router]]
             [cheshire.core :as json]
-            [ring.adapter.jetty9 :as jetty]
+            [ring.adapter.undertow :refer [run-undertow]]
+            [ring.adapter.undertow.websocket :as ws]
             [ring.middleware.content-type :refer [wrap-content-type]]
             [ring.middleware.json :refer [wrap-json-body]]
             [ring.middleware.not-modified :refer [wrap-not-modified]]
@@ -17,22 +18,13 @@
                     (json/generate-string))]
     (future
       (doseq [ws @ws-clients]
-        (jetty/send! ws message))))
+        (ws/send message ws))))
   (resp/created ""))
 
-(defn ws-handler [upgrade-request]
-  (let [provided-subprotocols (:websocket-subprotocols upgrade-request)
-        provided-extensions (:websocket-extensions upgrade-request)]
-    {:on-connect (fn [ws] (swap! ws-clients conj ws))
-     :on-close (fn [ws _status-code _reason] (swap! ws-clients disj ws))
-     :on-ping (fn [ws payload] (jetty/send! ws payload))
-     :on-error (fn [ws _e] (swap! ws-clients disj ws))
-     :subprotocol (first provided-subprotocols)
-     :extentions provided-extensions}))
-
-(defn ws-upgrade-handler [request]
-  (when (jetty/ws-upgrade-request? request)
-    (jetty/ws-upgrade-response ws-handler)))
+(defn ws-handler [_request]
+  {:undertow/websocket
+   {:on-open #(swap! ws-clients conj (:channel %))
+    :on-close-message #(swap! ws-clients disj (:channel %))}})
 
 (def handler
   (router
@@ -45,7 +37,7 @@
    (-> (POST "/api/messages" post-messages-handler)
        (wrap-json-body {:keywords? true}))
 
-   (ANY "/ws" ws-upgrade-handler)))
+   (ANY "/ws" ws-handler)))
 
 (defn -main [& _args]
-  (jetty/run-jetty handler {:port 8080}))
+  (run-undertow handler {:port 8080}))
